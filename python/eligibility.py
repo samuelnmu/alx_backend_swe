0802db3f9af6f6c072666f1bb12083207376f3c5
samuelnmu <samuelnmu@gmail.com>
2025-05-27 08:49:20 +0300

    Eligibility program commit

---
+++ python/eligibility.py
@@ -0,0 +1,11 @@
+name = input("Enter your Full name: ")
+age = int(input("Enter your age: "))
+
+
+ageLimit = 18
+if age < ageLimit:
+    print("You are not of legal age!")
+else:
+    age >= ageLimit
+    id = int(input("Enter your ID: "))
+    print("Welcome", name)
