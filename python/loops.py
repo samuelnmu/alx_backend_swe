a39fbbe4e66979fdd8ab170d07f688be8eeac531
samuelnmu <samuelnmu@gmail.com>
2025-06-02 09:36:35 +0300

    Loops program 1st trial

---
+++ python/loops.py
@@ -0,0 +1,13 @@
+name = "Samuel"
+
+for letter in name:
+    print(letter)
+    
+fruits = ["Mango","Banana","Orange","Apple"]
+
+for fruit in fruits:
+    print(fruit)
+    
+for number in range(1,10):
+    print(number)
+    
