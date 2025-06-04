d9bd8fd7baa8fdaa505f3ec4dcc74245a0718822
samuelnmu <samuelnmu@gmail.com>
2025-06-04 11:02:23 +0300

    Dynamic user commit

---
+++ python/functions/default.py
@@ -0,0 +1,11 @@
+def greet(name="World!"):
+    print(f"Hello, {name}")
+greet()
+
+def greeting(name = " "):
+    name = input("Name: ")
+    print(f"Hello {name}")
+greeting()
+
+name = input("Name: ")
+print(f"Hello, {name}")
